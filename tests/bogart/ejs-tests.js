02222993ce860710f43b297054a3b0ea84fd8f7a
Nathan <nathan@Planck.(none)>
2009-09-03 00:30:40 -0400

    contentFor now works a la rails erb templates

--- tests/bogart/ejs-tests.js
+++ tests/bogart/ejs-tests.js
@@ -12,3 +12,15 @@ exports.testRenderLayout = function() {
     var result = layoutRenderer.render(viewEJS);
     assert.isEqual("<html><body><span>Nathan</span></body></html>", result);
 };
+
+exports.testContentFor = function(){
+    var layout = "<html><head><%= hold('end_of_head') %></head><body><%= hold() %></body></html>";
+    var view = "<% contentFor('end_of_head', function() { %><title>Hello</title><% }); %>World";
+
+    var viewEJS = new EJS({text:view});
+    var layoutRenderer = new EjsLayoutRenderer(new EJS({text: layout}));
+
+    var result = layoutRenderer.render(viewEJS);
+
+    assert.isEqual("<html><head><title>Hello</title></head><body>World</body></html>", result);
+};
