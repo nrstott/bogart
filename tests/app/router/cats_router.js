f3c69b65c75f9da0ea5042bb50b3334e9816fa50
Nathan <nathan@Planck.(none)>
2009-09-12 23:56:20 -0400

    changed semantics of how auto-loaded routers are declared to make it more explicit, less magic

--- tests/app/router/cats_router.js
+++ tests/app/router/cats_router.js
@@ -1,9 +1,11 @@
-GET("/", function() {
-    this.response.write("index");
-    return this.response.finish();
-});
+Bogart.Router.extend("CatsRouter", function() {
+    this.GET("/", function() {
+        this.response.write("index");
+        return this.response.finish();
+    });
 
-GET("/new", function() {
-    this.response.write("new");
-    return this.response.finish();
+    this.GET("/new", function() {
+        this.response.write("new");
+        return this.response.finish();
+    });
 });
