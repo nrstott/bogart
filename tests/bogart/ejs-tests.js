4ab0cf93095cd8bc8389b66bedeb89e69294a450
Nathan <nathan@Planck.(none)>
2009-09-25 13:58:45 -0400

    form_for now renders a hidden input for _method if method is put or delete

--- tests/bogart/ejs-tests.js
+++ tests/bogart/ejs-tests.js
@@ -1,13 +1,14 @@
 var EJS = require("ejs").EJS;
 var assert = require("test/assert");
 var EjsLayoutRenderer = require("../../lib/bogart-ejs").EjsLayoutRenderer;
+var MockRequest = require("jack/mock").MockRequest;
 
 exports.testRenderLayout = function() {
     var layout = "<html><body><%= hold() %></body></html>";
     var view = "<span>Nathan</span>";
 
     var viewEJS = new EJS({text: view});
-    var layoutRenderer = new EjsLayoutRenderer(new EJS({text: layout}));
+    var layoutRenderer = new EjsLayoutRenderer(new EJS({text: layout}), MockRequest.envFor(null, "", {}));
 
     var result = layoutRenderer.render(viewEJS);
     assert.isEqual("<html><body><span>Nathan</span></body></html>", result);
@@ -18,9 +19,22 @@ exports.testContentFor = function(){
     var view = "<% content_for('end_of_head', function() { %><title>Hello</title><% }); %>World";
 
     var viewEJS = new EJS({text:view});
-    var layoutRenderer = new EjsLayoutRenderer(new EJS({text: layout}));
+    var layoutRenderer = new EjsLayoutRenderer(new EJS({text: layout}), MockRequest.envFor(null, "", {}));
 
     var result = layoutRenderer.render(viewEJS);
 
     assert.isEqual("<html><head><title>Hello</title></head><body>World</body></html>", result);
+};
+
+exports["test form_for with put method renders hidden field _method"] = function() {
+    var layout = "<html><body><%= hold() %></body></html>";
+    var view = "<span>test</span>" +
+               "<% form_for({ hello: 'word' }, '/', { method: 'put' }, function(f) {}) %>";
+
+    var viewEJS = new EJS({text:view});
+    var layoutRenderer = new EjsLayoutRenderer(new EJS({text: layout}), MockRequest.envFor(null, "", {}));
+
+    var result = layoutRenderer.render(viewEJS);
+
+    assert.isTrue(/<input id=['"]_method['"] value=['"]put['"] type=['"]hidden['"] name=['"]_method['"] \/>/g.test(result), result);
 };
