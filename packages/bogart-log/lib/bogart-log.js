6a05ce38bf27d999f5dd00e8c8396c8285437081
Nathan <nathan@Planck.(none)>
2009-09-26 18:32:39 -0400

    added .log extension to the bogart log file

--- packages/bogart-log/lib/bogart-log.js
+++ packages/bogart-log/lib/bogart-log.js
@@ -9,7 +9,7 @@ var configDir = fs.join(bogartRoot, "config");
 var configFile = fs.join(configDir, "log.json");
 
 var logDir = fs.join(bogartRoot, "log");
-var logFile = fs.join(logDir, bogartEnvironment.toLowerCase());
+var logFile = fs.join(logDir, bogartEnvironment.toLowerCase() + ".log");
 
 if (!fs.exists(logDir))
     fs.mkdir(logDir);
