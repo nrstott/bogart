9a5c8bbb6615c4b5de4abd877ed1795137e9a07d
nrstott <nrstott@gmail.com>
2010-07-22 14:21:47 -0700

    added support for rendering mustache templates

--- test/view.test.js
+++ test/view.test.js
@@ -3,9 +3,15 @@ var
   view   = require('../lib/view'),
   jsgi   = require('jsgi'),
   sys    = require('sys'),
-  when   = require('jsgi/promise').when
+  when   = require('jsgi/promise').when,
+  rootRequest = {
+    pathInfo: '/',
+    method: 'GET',
+    jsgi: { version: [0, 3] },
+    env: {}
+  }
 
-exports['test render'] = function(assert, beforeExit) {
+exports['test render haml'] = function(assert, beforeExit) {
   var app = 
     bogart.app(function(show) {
       
@@ -16,12 +22,7 @@ exports['test render'] = function(assert, beforeExit) {
       })
     })  
 
-  var resp = app({
-    pathInfo: '/',
-    method: 'GET',
-    jsgi: { version: [0, 3] },
-    env: {}
-  })
+  var resp = app(rootRequest)
 
   when(resp, 
     function(val) {
@@ -32,5 +33,28 @@ exports['test render'] = function(assert, beforeExit) {
           sys.puts('assert body')
 	  assert.equal(body, ['<h1>Hello World</h1>'])
         })
+    })
+}
+
+exports['test render mustache'] = function(assert, beforeExit) {
+  var app =
+    bogart.app(function(show) {
+      this.setting('views', __dirname + '/fixtures')
+
+      show('/', function(req, resp) {
+	return resp.render('index.mustache')
+      })
+    })
+
+  var resp = app(rootRequest)
+
+  when(resp,
+    function(val) {
+      var body = ''
+      when(val.body.forEach(function(x) { body += x }),
+	function() {
+	  sys.puts('assert body')
+	  assert.equal(body, ['<h1>Hello World from Mustache</h1>\n'])
+	})
     })
 }
