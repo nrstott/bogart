eeb76c79d72aae1aa2c05c9fc19d0a08e89ecd29
nrstott <nrstott@gmail.com>
2010-08-10 09:04:59 -0700

    added ';'

--- examples/mustache-layout/app.js
+++ examples/mustache-layout/app.js
@@ -7,8 +7,8 @@ var app = bogart.app(function(show) {
   this.setting('views', path.join(__dirname, 'views')).setting('viewEngine', 'mustache')
 
   show('/', function(req, res) {
-    return res.render('index.html', { locals: { description: 'This is content' } })
-  })
-})
+    return res.render('index.html', { locals: { description: 'This is content' } });
+  });
+});
 
-jsgi.start(app)
+jsgi.start(app);
