6fcc8ef8fda99fe745ea0bc799bdbab29f8074a4
nrstott <nrstott@gmail.com>
2010-08-20 14:15:47 -0700

    more testing and opts for bogart.html

--- test/response-helpers.test.js
+++ test/response-helpers.test.js
@@ -26,6 +26,52 @@ exports.json = {
   }
 };
 
+exports.error = {
+  "should have status 500": function() {
+    var resp = bogart.error();
+    
+    assert.equal(500, resp.status);
+  },
+  "should have status 403": function() {
+    var resp = bogart.error("", { status: 403 });
+    
+    assert.equal(403, resp.status);
+  }
+};
+
+exports.html = {
+  "should have status 200": function() {
+    var resp = bogart.html();
+    
+    assert.equal(200, resp.status);
+  },
+  "should have status 404": function() {
+    var resp = bogart.html("", { status: 404 });
+    
+    assert.equal(404, resp.status);
+  },
+  "should have HTML": function() {
+    var
+      str = "Hello World",
+      resp = bogart.html(str);
+    
+    assert.equal(str, resp.body.join());
+  },
+  "should be text/html": function() {
+    var
+      resp = bogart.html();
+    
+    assert.equal("text/html", resp.headers["content-type"])
+  },
+  "should have content-length 5": function() {
+    var
+      str = "hello",
+      resp = bogart.html(str);
+    
+    assert.equal(5, resp.headers["content-length"]);
+  }
+}
+
 if(require.main == module) {
   require("patr/runner").run(exports);
 }
