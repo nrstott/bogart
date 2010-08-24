ec6800cd0d3e23f4a5bb91429a699199f9c9f874
Davis Clark <davis@ubuntu.(none)>
2010-08-24 15:17:19 -0700

    added example with alternative routing implementation to test performance change

--- examples/alternative-routing/app.js
+++ examples/alternative-routing/app.js
@@ -1,26 +1,27 @@
-var bogart = require('../lib/bogart');
+var
+   bogart =       require('../../lib/bogart'),
+   router =       require('./alt-router'),
+   sys =          require('sys');
 
 var config = function(show, create, update, destroy) {
-  show('/hello/:name', function(req, name) {
-    return bogart.html('Hello '+name);
+  show('/hello/@firstname/@lastname', function(req, fname, lname) {
+    return bogart.html('Hello '+fname + " "+lname);
   });
-  
+
   show('/stream', function(req) {
     var streamer = bogart.stream();
-    
+
     setInterval(function() {
       var currentTime = new Date();
       streamer(currentTime.getHours()+':'+currentTime.getMinutes()+':'+currentTime.getSeconds()+"\n");
     }, 10);
-    
+
     setTimeout(function() {
       streamer.end();
     }, 10000);
-    
+
     return streamer.respond();
   });
 };
 
-
-
-bogart.start(bogart.router(config));
+bogart.start(router.init(config));
