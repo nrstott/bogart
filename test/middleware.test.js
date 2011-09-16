3470c72b97b1d3f505d659911d8ee4369f89c98b
nrstott <nrstott@gmail.com>
2011-09-16 07:20:00 -0500

    add tests for middleware

---
+++ test/middleware.test.js
@@ -0,0 +1,82 @@
+var bogart = require('../lib/bogart')
+  , Q      = require("promised-io/lib/promise")
+  , assert = require('assert');
+
+exports["test parses JSON"] = function(beforeExit) {
+  var forEachDeferred = Q.defer()
+    , app
+    , body
+    , headers = { 'content-type': 'application/json' }
+    , request = { headers: headers }
+    , processedReq;
+
+  body = {
+    forEach: function(callback) {
+      callback(JSON.stringify({ a: '1' }));
+      return forEachDeferred;
+    }
+  };
+
+  request.body = body;
+
+  app = bogart.middleware.ParseJson(function(req) {
+    processedReq = req;
+  });
+
+  app(request);
+
+  forEachDeferred.resolve();
+
+  beforeExit(function() {
+    assert.ok(processedReq !== undefined);
+    assert.equal('1', processedReq.body.a);
+  })
+};
+
+exports["test parses form"] = function(beforeExit) {
+  var forEachDeferred = Q.defer()
+    , app
+    , body
+    , headers = { 'content-type': 'application/x-www-form-urlencoded' }
+    , request = { headers: headers }
+    , processedReq;
+  
+  body = {
+    forEach: function(callback) {
+      callback('a=1');
+      return forEachDeferred;
+    }
+  };
+
+  request.body = body;
+
+  app = bogart.middleware.ParseForm(function(req) {
+    processedReq = req;
+  });
+
+  app(request);
+
+  forEachDeferred.resolve();
+
+  beforeExit(function() {
+    assert.ok(processedReq !== undefined);
+    assert.equal('object', typeof processedReq.body, 'Body should be an object');
+    assert.equal('1', processedReq.body.a);
+  });
+};
+
+exports["test method override"] = function(beforeExit) {
+  var request = { method: 'POST', env: {} }
+    , headers = { 'content-type': 'text/html' }
+    , app;
+
+  request.body    = { _method: 'PUT' };
+  request.headers = headers;
+
+  app = bogart.middleware.MethodOverride(function(req) {});
+  app(request);
+
+  beforeExit(function() {
+    assert.equal('PUT', request.method, 'Should change method to PUT');
+  });
+};
