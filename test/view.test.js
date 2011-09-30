6a452efc57255666dc9e8d9019ed1daa53d39b58
nrstott <nrstott@gmail.com>
2011-09-30 11:32:16 -0500

    moved jade and haml to plugins, made view engine more extensible

--- test/view.test.js
+++ test/view.test.js
@@ -5,19 +5,6 @@ var bogart = require('../lib/bogart')
   , assert = require('assert')
   , fixturesDir = process.cwd() + '/test/fixtures';
 
-exports['test render haml'] = function(beforeExit) {
-  var viewEngine = bogart.viewEngine('haml', __dirname+'/fixtures')
-    , renderedText;
-  
-  when(viewEngine.render('index.haml', { layout: false }), function(str) {
-    renderedText = str;
-  });
-
-  beforeExit(function() {
-    assert.equal(renderedText, '<h1>Hello World</h1>');
-  });
-};
-
 exports['test render mustache'] = function(beforeExit) {
   var viewEngine = bogart.viewEngine('mustache', __dirname+'/fixtures')
     , renderedText;
@@ -30,16 +17,3 @@ exports['test render mustache'] = function(beforeExit) {
     assert.equal(renderedText, '<h1>Hello World from Mustache</h1>\n');
   });
 };
-
-exports['test render jade'] = function(beforeExit) {
-  var viewEngine = bogart.viewEngine('jade', __dirname+'/fixtures')
-    , renderedText;
-  
-  when(viewEngine.render('index.jade', { layout: false }), function(str) {
-    renderedText = str;
-  });
-
-  beforeExit(function() {
-    assert.equal(renderedText, '<h1>Hello World from Jade</h1>');
-  });
-}
