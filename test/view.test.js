da46b0d09e3c47efc8db593b8d7000d31b16cf27
nrstott <nrstott@gmail.com>
2011-10-03 15:54:36 -0500

    added support for partials to the mustache template renderer

--- test/view.test.js
+++ test/view.test.js
@@ -1,12 +1,13 @@
-var bogart = require('../lib/bogart')
-  , view   = require('../lib/view')
-  , jsgi   = require('jsgi')
-  , when   = require('promised-io/lib/promise').when
-  , assert = require('assert')
-  , fixturesDir = process.cwd() + '/test/fixtures';
+var bogart       = require('../lib/bogart')
+  , view         = require('../lib/view')
+  , jsgi         = require('jsgi')
+  , when         = require('promised-io/lib/promise').when
+  , assert       = require('assert')
+  , path         = require('path')
+  , fixturesPath = path.join(__dirname, 'fixtures');
 
 exports['test render mustache'] = function(beforeExit) {
-  var viewEngine = bogart.viewEngine('mustache', __dirname+'/fixtures')
+  var viewEngine = bogart.viewEngine('mustache', fixturesPath)
     , renderedText;
   
   when(viewEngine.render('index.mustache', { layout: false }), function(str) {
@@ -15,5 +16,18 @@ exports['test render mustache'] = function(beforeExit) {
 
   beforeExit(function() {
     assert.equal(renderedText, '<h1>Hello World from Mustache</h1>\n');
+  });
+};
+
+exports['test render mustache with partials'] = function(beforeExit) {
+  var viewEngine = bogart.viewEngine('mustache', fixturesPath)
+    , renderedText;
+  
+  when(viewEngine.render('partial-test.mustache', { layout: false, locals: { greeting: {} }, partials: { greeting: 'greeting.mustache' } }), function(str) {
+    renderedText = str;
+  });
+
+  beforeExit(function() {
+    assert.equal(renderedText, '<h1>Hello World from Mustache</h1>\n<p>With Partial</p>');
   });
 };
