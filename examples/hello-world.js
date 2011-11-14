c57b3096f75b83913a8594dc383876966d04432f
nrstott <nrstott@gmail.com>
2011-11-14 09:30:04 -0600

    simplified hello-world example

--- examples/hello-world.js
+++ examples/hello-world.js
@@ -4,23 +4,6 @@ var config = function(show, create, update, destroy) {
   show('/hello/:name', function(req, name) {
     return bogart.html('Hello '+name);
   });
-
-  show('/stream', function(req) {
-    var streamer = bogart.stream();
-    
-    setInterval(function() {
-      var currentTime = new Date();
-      streamer(currentTime.getHours()+':'+currentTime.getMinutes()+':'+currentTime.getSeconds()+"\n");
-    }, 10);
-    
-    setTimeout(function() {
-      streamer.end();
-    }, 10000);
-    
-    return streamer.respond();
-  });
 };
 
-
-
-bogart.start(bogart.router(config), {port:1337});
+bogart.start(bogart.router(config));
