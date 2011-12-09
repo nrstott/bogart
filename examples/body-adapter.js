2d09848db3465e38dd12f9133aa4f787dba73b9b
nrstott <nrstott@gmail.com>
2011-12-09 17:01:33 -0600

    added example of returning a buffer from a Bogart route to examples/body-adapter.js

--- examples/body-adapter.js
+++ examples/body-adapter.js
@@ -10,9 +10,11 @@ function HelloWorldStream() {
   this.readable = true;
 
   process.nextTick(function() {
-    self.emit('data', 'Hello');
+    self.emit('data', '<html><head><title>Stream</title><body>');
     process.nextTick(function() {
-      self.emit('data', ' World');
+      self.emit('data', 
+        'This is an example of a Bogart route that returns a stream. '+
+        '<a href="/buffer">See a route that returns a Buffer</a></body></html>');
       self.emit('end');
     });
   });
@@ -23,6 +25,11 @@ util.inherits(HelloWorldStream, Stream);
 var router = bogart.router();
 router.get('/', function() {
   return new HelloWorldStream();
+});
+
+router.get('/buffer', function() {
+  return new Buffer('<html><head><title>Buffer</title></head>' +
+    '<body>This is an example of a Bogart route that returns a buffer.</body></html>');
 });
 
 var app = bogart.app();
