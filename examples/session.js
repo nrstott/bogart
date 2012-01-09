396dad7caf140ff491696a7043efbaabf8250653
nrstott <nrstott@gmail.com>
2012-01-09 15:38:27 -0600

    updated session.js for new bogart best practices

--- examples/session.js
+++ examples/session.js
@@ -1,25 +1,27 @@
 var bogart = require('../lib/bogart');
 
+var router = bogart.router();
 
-var config = function(show, create, update, destroy) {
-  show('/', function(req) {
-    req.session("foo", "bar");
+router.get('/', function(req) {
+  req.session("foo", "bar");
 
-    var session = "Session: <br /><ul>";
+  var session = "Session: <br /><ul>";
 
-    req.session.keys().forEach(function(key) {
-      session += "<li>"+key+": "+req.session(key)+"</li>";
-    });
-
-    session += "</ul>";
-
-    return bogart.html(session);
+  req.session.keys().forEach(function(key) {
+    session += "<li>"+key+": "+req.session(key)+"</li>";
   });
-};
+
+  session += "</ul>";
+
+  return bogart.html(session);
+});
 
 var sessionConfig = {
   lifetime: 600
 };
 
-var app = bogart.middleware.Session(sessionConfig, bogart.router(config));
-bogart.start(app, {port:1337});
+var app = bogart.app();
+app.use(bogart.middleware.session(sessionConfig));
+app.use(router);
+
+app.start();
