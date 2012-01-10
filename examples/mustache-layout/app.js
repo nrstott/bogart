5cb58db971a403e059f64fec0c7485539afef846
nrstott <nrstott@gmail.com>
2012-01-10 13:15:43 -0600

    updated mustache-layout example

--- examples/mustache-layout/app.js
+++ examples/mustache-layout/app.js
@@ -1,13 +1,25 @@
-var
-  bogart = require('../../lib/bogart'),
-  path   = require('path')
+/**
+ * Include Modules.
+ */
+var bogart = require('../../lib/bogart')
+  , path   = require('path');
 
-var app = bogart.router(function(show) {
-  var viewEngine = bogart.viewEngine('mustache', path.join(bogart.maindir(), 'views'));
+// Create a Bogart ViewEngine.
+var viewEngine = bogart.viewEngine('mustache', path.join(bogart.maindir(), 'views'));
 
-  show('/', function(req, res) {
-    return viewEngine.respond('index.html', { locals: { description: 'This is content' } });
-  });
+// Create a Bogart Router.
+var router = bogart.router()
+
+// Add a route to handle the path "/" to the router.
+router.get('/', function(req, res) {
+  return viewEngine.respond('index.html', { locals: { description: 'This is content' } });
 });
 
-bogart.start(app);
+// Create a Bogart Application.
+var app = bogart.app();
+
+// Add the router to the Application.
+app.use(router);
+
+// Start the Application.
+app.start();
