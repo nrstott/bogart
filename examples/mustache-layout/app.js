2f51b4718a1c4e9e08eb436700e52c2557e1be01
Nathan <nathan.stott@whiteboard-it.cm>
2013-06-17 11:05:55 -0500

    updated mustache-layout example

--- examples/mustache-layout/app.js
+++ examples/mustache-layout/app.js
@@ -1,13 +1,12 @@
-var
-  bogart = require('../../lib/bogart'),
-  path   = require('path')
+var bogart = require('../../lib/bogart')
+  , path   = require('path');
 
-var app = bogart.router(function(show) {
-  var viewEngine = bogart.viewEngine('mustache', path.join(bogart.maindir(), 'views'));
+var viewEngine = bogart.viewEngine('mustache', path.join(bogart.maindir(), 'views'));
 
-  show('/', function(req, res) {
-    return viewEngine.respond('index.html', { locals: { description: 'This is content' } });
-  });
+var router = bogart.router();
+
+router.get('/', function(req, res) {
+  return viewEngine.respond('index.html', { locals: { description: 'This is content' } });
 });
 
-bogart.start(app);
+bogart.start(router);
