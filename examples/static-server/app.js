0770602cabe8f81ffd903c132dede8f77b25b5e8
Nathan <nathan.stott@whiteboard-it.cm>
2013-06-17 11:04:31 -0500

    updated static-server example

--- examples/static-server/app.js
+++ examples/static-server/app.js
@@ -1,10 +1,10 @@
 var
   bogart = require("../../lib/bogart");
 
-var router = bogart.router(function(get) {
-  get("/", function(req) {
-    return bogart.html("<html><body><img src='/images/ninja-cat.jpg' /></body></html>");
-  });
+var router = bogart.router();
+
+router.get("/", function(req) {
+  return bogart.html("<html><body><img src='/images/ninja-cat.jpg' /></body></html>");
 });
 
 var root = require("path").join(__dirname, "public");
