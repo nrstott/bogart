c7b08dc7e02488e7f7c261a313afd9bd91aba183
Nathan <nathan.stott@whiteboard-it.cm>
2013-07-03 16:56:35 -0500

    Improved the flash middleware example

--- examples/flash-test.js
+++ examples/flash-test.js
@@ -1,23 +1,25 @@
 var bogart = require('../lib/bogart');
 
-
-var config = function(show, create, update, destroy) {
-  show('/flash', function(req) {
-    var existing  = "Existing Flash:"+ req.flash("foo") +"<br />";
-    req.flash("foo", Math.random() * 10);
-    return bogart.html(existing);
-  });
-};
-
-//the following config options are optional, defaults are assumed
 var flashConfig = {
-  options: {
-    dataProvider: {
-      encryptionKey:"db0031e2-04e7-11e1-86ad-000c290196f7"
-    },
-    idProvider: {}
-  }
+  encryptionKey: 'db0031e2-04e7-11e1-86ad-000c290196f7'
 };
 
-var app = bogart.middleware.Flash(flashConfig, bogart.router(config));
-bogart.start(app, {port:1337});
+var router = bogart.router();
+router.get('/', function (req) {
+  var html  = '<html><head><title>Bogart Flash</title></head><body><h1>Bogart Flash Middleware</h1>';
+  html += '<p>A random number has been inserted into flash. ' +
+    'On the first request, the value should be undefined. Subsequent requests should ' +
+    'display the random number set on the previous request. ' +
+    'Refresh the page to see the flash value change.</p>' +
+    'Existing Flash: '+ req.flash('foo') +'<br />';
+
+  req.flash('foo', Math.random() * 10);
+
+  return bogart.html(html);
+});
+
+var app = bogart.app();
+app.use(bogart.middleware.flash(flashConfig));
+app.use(router);
+
+app.start();
