8863a92d26d06884e1365682016cb855d1c4efe1
Nathan <nathan.stott@whiteboard-it.cm>
2013-07-03 16:59:38 -0500

    Updates the gzip example to use bogart#app

--- examples/gzip-response.js
+++ examples/gzip-response.js
@@ -5,7 +5,8 @@ router.get('/', function() {
   return bogart.html('<h1>This response is compressed by the Deflate middleware!</h1>');
 });
 
-var Gzip = bogart.middleware.Gzip;
-var app = Gzip(router);
+var app = bogart.app();
+app.use(bogart.middleware.gzip);
+app.use(router);
 
-bogart.start(app);
+app.start();
