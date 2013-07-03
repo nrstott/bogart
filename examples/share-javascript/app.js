280a5a967c0453625cdfa056148b3e49b0070c4b
Nathan <nathan.stott@whiteboard-it.cm>
2013-07-03 17:02:10 -0500

    Updates the share-javascript example to use bogart#app

--- examples/share-javascript/app.js
+++ examples/share-javascript/app.js
@@ -1,20 +1,24 @@
 var bogart = require('../../lib/bogart');
 
-var router = bogart.router()
-  , viewEngine = bogart.viewEngine('mustache');
+var viewEngine = bogart.viewEngine('mustache');
 
 viewEngine.share(new Date(), 'now');
+
 viewEngine.share({
 	app: 'JavaScript Sharing Example',
 	framework: 'Bogart',
 	author: 'Nathan Stott'
 }, 'settings');
+
 viewEngine.share(function() {
 	return 'Shared on the server, executed on the client';
 }, 'fn');
 
+var router = bogart.router()
 router.get('/', function(req) {
   return viewEngine.respond('index.html', { locals: { title: 'Hello World' }, layout: false });
 });
 
-bogart.start(router);
+var app = bogart.app();
+app.use(router);
+app.start();
