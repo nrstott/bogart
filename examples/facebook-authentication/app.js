3a3299338e5091fc815f5c55974d6c594ca13d02
nrstott <nrstott@gmail.com>
2014-03-05 16:31:01 -0600

    update facebook middleware and example, fix error of returning router without nextApp passed in oauth2 middleware

--- examples/facebook-authentication/app.js
+++ examples/facebook-authentication/app.js
@@ -1,67 +1,35 @@
+const PORT = 1337;
+
+var path = require('path');
 var bogart = require('../../lib/bogart');
 
-var FACEBOOK_APP_ID = "ENTER_YOUR_FACEBOOK_APP_ID"
-var FACEBOOK_APP_SECRET = "ENTER_YOUR_FACEBOOK_APP_SECRET";
+var config = require('./config.json');
 
-// Example oauth2 Strategy
-//  would be called like: var fbauth = new bogart.middleware.facebook(facebook_strategy, router);
-var facebook_strategy = {
-    authorizationURL: 'https://www.facebook.com/dialog/oauth',
-    tokenURL: 'https://graph.facebook.com/oauth/access_token',
-    resourceURL: 'https://graph.facebook.com/me',
-    clientId: FACEBOOK_APP_ID,
-    clientSecret: FACEBOOK_APP_SECRET,
-    redirectURL: '/profile',
-    parse: function(body) {
-        o = JSON.parse(body);
-
-        var profile = {
-            provider: 'facebook'
-        };
-        profile.id = o.id;
-        profile.username = o.username;
-        profile.displayName = o.name;
-        profile.name = {
-            familyName: o.last_name,
-            givenName: o.first_name,
-            middleName: o.middle_name
-        };
-        profile.gender = o.gender;
-        profile.profileUrl = o.link;
-        profile.emails = [{
-            value: o.email
-        }];
-
-        return profile;
-    }
+var facebookConfig = {
+  clientId: config.appId,
+  clientSecret: config.secret,
+  host: 'http://localhost:'+PORT
 };
-
-var facebook_simple = {
-	clientId: FACEBOOK_APP_ID,
-    clientSecret: FACEBOOK_APP_SECRET,
-    host: 'http://localhost:8089'
-}
 
 var router = bogart.router();
 router.get('/profile', function(req) {
-    return bogart.html(JSON.stringify(req.session('profile')));
+  return bogart.html(JSON.stringify(req.session('profile')));
 });
 
-var fbauth = new bogart.middleware.facebook(facebook_simple, router);
-
-var frontRouter = bogart.router(fbauth);
+var frontRouter = bogart.router();
 frontRouter.get('/', function(req) {
-    return bogart.html('<a href="/profile">Profile</a> | <a href="/login">Login</a>');
+  return bogart.html('<a href="/profile">Profile</a> | <a href="/login">Login</a>');
 });
 
 frontRouter.get('/login', function(req) {
-    return bogart.html('<a href="/auth/login?returnUrl=/profile"><img src="login-with-facebook.png" width="154" height="22"></a>');
+  return bogart.html('<a href="/auth/login?returnUrl=/profile"><img src="login-with-facebook.png" width="154" height="22"></a>');
 });
 
-var app = bogart.middleware.Session(null, frontRouter);
-app = bogart.middleware.Flash(null,app);
-app = bogart.middleware.Directory(require("path").join(__dirname, "public"), app);
+var app = bogart.app();
+app.use(bogart.middleware.session({ secret: 'ABC123' }));
+app.use(bogart.middleware.directory(path.join(__dirname, 'public')));
+app.use(frontRouter);
+app.use(bogart.middleware.facebook(facebookConfig));
+app.use(router);
 
-bogart.start(app, {
-    port: 8089
-});
+app.start(PORT);
